3222c75f3daf654a3504d204d96ef268fd5edbdc
Abhi Talati <forfoxy027@gmail.com>
2025-11-26 18:34:46 +0530

    after update

--- js/addsnip.js
+++ js/addsnip.js
@@ -6,19 +6,29 @@ let code = document.getElementById("code");
 let tags = document.getElementById("tags");
 const save = document.getElementById("saveBtn");
 const cancle= document.getElementById("cancelBtn");
-localStorage.setItem("Data" , "there it is ")
+
+// using data to check/test is it works or not 
+const data = [
+    {c_id : 1 , tittle : `tittle` , code : `code` , tags : `tags` , lang : `lang` },
+    {c_id : 2 , tittle : `tittle` , code : `code` , tags : `tags` , lang : `lang` },
+]
+const newd = JSON.stringify(data);
+const setdata = localStorage.setItem("Data" , newd);
+
+const wrealdata = localStorage.getItem("Data");
+const realdata = JSON.parse(wrealdata);
 
 function checkid(){
-    let Lid = realAdd.length;
+    let Lid = realdata.length;
     let index = Lid - 1;
     console.log(`index is ${index}`);
     console.log(`Lid is ${Lid}`);
 }
 
 save.onclick = ()=>{
-    checkdata();
-    // checkid();
-    // console.log(tittle.value);
+    // checkdata();
+    checkid();
+    console.log(tittle.value);
 }
 
 cancle.onclick = () => {
@@ -26,6 +36,13 @@ cancle.onclick = () => {
 }
 
 function checkdata() {
-    const realdata = JSON.parse(localStorage.getItem("Data"));
-    console.log(realdata);
+    if(realdata){
+        let lastid = realdata.length;
+        let newcid = [lastid].tittle;
+        console.log(lastid);
+        console.log(realdata);
+    }
+    else{
+        console.log("it doesnt exists")
+    }
 }
