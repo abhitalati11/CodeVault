80c1a95f0c524b4859640cbef5aa5c1109fb9f61
Abhi Talati <forfoxy027@gmail.com>
2025-12-01 17:36:41 +0530

    update 1.0

--- js/addsnip.js
+++ js/addsnip.js
@@ -7,42 +7,10 @@ let tags = document.getElementById("tags");
 const save = document.getElementById("saveBtn");
 const cancle= document.getElementById("cancelBtn");
 
-// using data to check/test is it works or not 
-const data = [
-    {c_id : 1 , tittle : `tittle` , code : `code` , tags : `tags` , lang : `lang` },
-    {c_id : 2 , tittle : `tittle` , code : `code` , tags : `tags` , lang : `lang` },
-]
-const newd = JSON.stringify(data);
-const setdata = localStorage.setItem("Data" , newd);
-
-const wrealdata = localStorage.getItem("Data");
-const realdata = JSON.parse(wrealdata);
-
-function checkid(){
-    let Lid = realdata.length;
-    let index = Lid - 1;
-    console.log(`index is ${index}`);
-    console.log(`Lid is ${Lid}`);
-}
-
 save.onclick = ()=>{
-    // checkdata();
-    checkid();
-    console.log(tittle.value);
+    console.log(tittle.value , lang.value , tags.value);
 }
 
 cancle.onclick = () => {
     window.close();
-}
-
-function checkdata() {
-    if(realdata){
-        let lastid = realdata.length;
-        let newcid = [lastid].tittle;
-        console.log(lastid);
-        console.log(realdata);
-    }
-    else{
-        console.log("it doesnt exists")
-    }
 }
