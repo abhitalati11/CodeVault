9ac904cb294c2c8c4b6a601bd8ffe67e79f419fb
Abhi Talati <forfoxy027@gmail.com>
2025-12-02 23:08:10 +0530

    update 1.0.1

--- js/addsnip.js
+++ js/addsnip.js
@@ -9,8 +9,37 @@ const cancle= document.getElementById("cancelBtn");
 
 save.onclick = ()=>{
     console.log(tittle.value , lang.value , tags.value);
+    checkdata();
 }
 
 cancle.onclick = () => {
     window.close();
+}
+
+function checkdata(){
+    if(localStorage.getItem("Data")){
+        const rawdata =localStorage.getItem("Data");
+        const realdata = JSON.parse(rawdata);
+
+        console.log(realdata);
+
+        let length = realdata.length;
+        let lastindex = length - 1;
+
+        console.log(lastindex);
+        console.log(realdata[lastindex] );
+
+        let newdata = {c_id : length + 1 , tittle : tittle.value , code : code.value , tags : tags.value, lang : lang.value}
+
+        realdata.push(newdata);
+        console.log(newdata);
+        console.log(realdata);
+
+        const nextdata = JSON.stringify(realdata);
+        localStorage.clear();
+        localStorage.setItem("Data" , nextdata);
+    }
+    else{
+        console.log("no data")
+    }
 }
