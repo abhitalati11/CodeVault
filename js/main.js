08095dee92090d89317a02fa72f66f1a9ffc8c98
Abhi Talati <forfoxy027@gmail.com>
2025-12-06 18:41:32 +0530

    update 1.1.1

--- js/main.js
+++ js/main.js
@@ -10,75 +10,65 @@ addbtn.onclick = function(){
 
 let snippetCard = document.getElementById(`cardS`);
 
-snippetCard.addEventListener(`click` , function(){
-    window.open("detail.html");
-});
+// snippetCard.addEventListener(`click` , function(){
+//     window.open("detail.html");
+// });
 
 // adding daynamic divs to homepage
 let sinppetgrid = document.getElementById(`snippet-grid`);
 
 function addingDiv(){
-    let newdiv = document.createElement(`div`);
-    newdiv.className = "snippet-card";
-    newdiv.id = "cards";
-
-    sinppetgrid.appendChild(newdiv);
-
-    let h3 = document.createElement("h3");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
-
-    h3.innerText = tittle;
-    p1.innerText = `Language: ${lang}`;
-    p2.innerText = `Tags: ${tags}`;
-
-    newdiv.appendChild(h3);
-    newdiv.appendChild(p1);
-    newdiv.appendChild(p2);
 }
 
 
 
 // getting local storage from device
 
-let code = localStorage.getItem(`Code`);
-let tittle = localStorage.getItem(`tittle`);
-let tags = localStorage.getItem(`Tags`);
-let lang = localStorage.getItem(`Langauge`);
+
+const rawdata = localStorage.getItem("Data");
+const realdata = JSON.parse(rawdata);
 
 
-if(checkLocal() , true){
-    console.log("works");
-    addingDiv();
+if(localStorage.getItem("Data")){
+    // console.log("works");
+    for(let i = 0 ; i < realdata.length ; i++){
+        console.log(realdata[i].tittle);
+        let code = realdata[i].code;
+        let tittle = realdata[i].tittle;
+        let tags = realdata[i].tags;
+        let lang = realdata[i].lang;
+
+        // adding div function
+        let newdiv = document.createElement(`div`);
+        newdiv.className = "snippet-card";
+        newdiv.id = "cards";
+
+        sinppetgrid.appendChild(newdiv);
+
+        let h3 = document.createElement("h3");
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+
+        h3.innerText = tittle;
+        p1.innerText = `Language: ${lang}`;
+        p2.innerText = `Tags: ${tags}`;
+
+        newdiv.appendChild(h3);
+        newdiv.appendChild(p1);
+        newdiv.appendChild(p2);
+
+
+    }
 }
 else{
     console.log("Dont know");
 }
-function checkLocal(){
+// function checkLocal(){
 
-if(code !== null && tittle !== null && tags !== null && lang !== null) {
-    console.log("it work");
-}
-else{
-    console.log("it doent work");
-}    
-}
-
-// for making js obj and store data in that 
-
-const data = [
-    {c_id : 1 , tittle : `${tittle}` , code : `${code}` , tags : `${tags}` , lang : `${lang}` },
-    {c_id : 2 , tittle : `${tittle}` , code : `${code}` , tags : `${tags}` , lang : `${lang}` },
-]
-
-// const dataString = JSON.stringify(data);
-
-// localStorage.setItem("Data" , dataString);
-
-// const realdata = JSON.parse(dataString);
-// console.log(realdata);
-
-// let lastid = realdata.length;
-// let lstIn = lastid - 1;
-// console.log(lastid);
-// console.log(lstIn);
+// if(code !== null && tittle !== null && tags !== null && lang !== null) {
+//     // console.log("it work");
+// }
+// else{
+//     // console.log("it doent work");
+// }    
+// }
